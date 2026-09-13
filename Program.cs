c3e1ceb166e446ec98e4b4f9cddb616922977bc0
Luilton <xxluiltonxx@gmail.com>
2026-09-12 21:06:56 -0400

    Creando clase maximo comun divisor

--- Program.cs
+++ Program.cs
@@ -3,10 +3,7 @@ using PrimeraWebApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 // base de datos
 builder.Services.AddDbContext<CamionerosDbContext>(options =>
